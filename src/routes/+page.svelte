ba7199ee2fc2e39dd34f537d44a3f25faf43a8c7
Keerthan <kkeerthan2005@gmail.com>
2024-10-07 22:41:33 +0530

    checkpoint: works. todo: extend the dataset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,79 @@
-<div class="transition-all duration-300 flex flex-col sm:flex-row justify-center items-center h-[100vh] bg-[url('/bliss.heic')] bg-no-repeat bg-cover bg-left-bottom">
-    <div class="h-full w-full flex flex-col sm:flex-row  sm:justify-center sm:items-center bg-neutral-900 backdrop-blur-sm bg-opacity-30 px-2 pt-2">
-        <div class="transition-all duration-300 flex  justify-center items-center backdrop-blur-2xl bg-opacity-20 sm:rounded-3xl rounded-2xl bg-neutral-800 w-full sm:w-[60vw] h-[200vh] sm:h-[90vh]">
-            <div class="transition-all duration-300 sm:ml-8">
-                <h1 class="transition-all duration-300 text-5xl sm:text-8xl font-caveat text-neutral-50 cursor-default selection:hidden ml-4">FortFolio under construction</h1>
-            </div>
-            <!--        <div class="w-1/2 flex align-middle mr-8">-->
-            <!--            <img src="bliss.heic" class="">-->
-            <!--        </div>-->
-        </div>
-
-        <div class="transition-all duration-300 flex justify-center items-center backdrop-blur-2xl bg-opacity-20 sm:rounded-3xl rounded-2xl bg-neutral-800 my-4 sm:ml-4 sm:w-[20vw] h-screen sm:h-[90vh]">
+<div class="flex transition-all duration-300 ease-in-out flex-row h-screen w-screen justify-center animated-gradient">
+    <div id="content" class="flex flex-col xl:flex-row w-full sm:w-[70vw] items-center md:justify-evenly justify-center">
+        <h1 class="text-cyan-100 text-5xl text-center font-extrabold">Sketch it up!</h1>
+        <br>
+        <div id="sketchPadContainer">
 
         </div>
+        <script src="/src/lib/draw.js"></script>
+        <script src="/src/lib/sketchPad.js"></script>
+        <script>
+
+            const data={
+                session:new Date().getTime(),
+                drawings:{},
+                label:null,
+            };
+
+            const sketchPad = new SketchPad(sketchPadContainer);
+            const labelBox = document.getElementById('labelBox');
+
+            async function recognize() {
+                if (sketchPad.paths.length === 0) {
+                    alert("Missing Label or Sketch");
+                    return;
+                }
+                data.label = labelBox.value;
+                data.drawings[data.label] = sketchPad.paths;
+
+                const response = await fetch('?/predict', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        strokes: sketchPad.paths,
+                    })
+                }).then(response => response.json());
+
+                console.log(response.data[40]);
+                if(response.data[40]==4) {
+                    labelBox.value = "Airplane";
+                } else if (response.data[42]==4) {
+                    labelBox.value = "Bicycle";
+                } else if (response.data[44]==4) {
+                    labelBox.value = "Car";
+                } else if (response.data[46]==4) {
+                    labelBox.value = "Banana";
+                }
+            }
+        </script>
     </div>
 </div>
+
+<style>
+    div {
+        font-family: Candara,serif;
+        /*font-weight: bold;*/
+        overscroll-behavior: none;
+    }
+
+    :global(body) {
+        --gradient-angle: 0deg;
+    }
+
+    .animated-gradient {
+        background-image: radial-gradient(circle at 70% 50%, #0072b3, #0040a1);
+        background-size: 200% 200%;
+        animation: gradient-animation 10s infinite ease-in-out;
+    }
+
+    @keyframes gradient-animation {
+        0% {
+            background-position: 0 0;
+        }
+        50% {
+            background-position: 100% 100%;
+        }
+        100% {
+            background-position: 0 0;
+        }
+    }
+</style>
